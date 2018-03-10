4f03b172f85277f76c5afe9464360af0db2bc81b
toyoda <masa.toyoda2525@gmail.com>
2018-03-10 15:23:58 +0900

    kutsu

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,37 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all
-  end  
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to "/items/#{@item.id}"
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to "/items"
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to "/items"
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :price, :seller_id, :description, :email, :image_url)
+  end
 end
